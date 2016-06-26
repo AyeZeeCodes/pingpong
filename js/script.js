d1adc7f0089ed41d8fdffd8a7e7c04d64c1d9e02
ayezee33 <ayezee33@gmail.com>
2016-06-26 09:03:09 -0700

    add function to iterate over number input and has the logic to output different text based on what the number is divisble by

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-
+function pingPong(number) {
+  var number;
+  output = "";
+  for (i = 1; i <= number; i++) {
+    output += logic(i) + '</br>';
+  }
+  return output;
+}
+function logic(number) {
+  if (number % 15===0) {
+    return 'pingpong';
+  } else if (number % 5===0){
+    return 'pong';
+  } else if (number % 3===0) {
+    return 'ping';
+  } else {
+    return number;
+  }
+}
 $(document).ready(function() {
   $("#ping").submit(function(event) {
     event.preventDefault();
